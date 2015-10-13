8abab1010949d9ae5a2decc79bf1caef8ddaaa36
Ignasi Marimon-Clos <ignasi@marimon-clos.com>
2015-10-13 19:19:37 +0200

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to word-wrap
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/word_wrap/core_test.clj
@@ -0,0 +1,7 @@
+(ns word-wrap.core-test
+  (:require [clojure.test :refer :all]
+            [word-wrap.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
