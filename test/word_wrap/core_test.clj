e4ac3b1cda431152f452b7f84f897483ae0b9a14
Ignasi Marimon-Clos <ignasi@marimon-clos.com>
2015-10-13 20:35:55 +0200

    Improves test docs

--- test/word_wrap/core_test.clj
+++ test/word_wrap/core_test.clj
@@ -4,17 +4,18 @@
 
 (deftest a-test
 
-  (testing "Word wrapping wrapper wrap"
+  (testing "Word wrapping  when input is shorter than line maxlength is identity"
     (is (=
           "hello"
           (wrap "hello" 100)
            ))
     )
 
-  (testing "Word wrapping wrapper wrap"
+  (testing "Word wrapping naive ignores white space"
     (is (=
           "hell\no wo\nrld"
           (wrap "hello world" 4)
            ))
     )
-)
+
+  )
